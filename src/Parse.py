80d0f929186c30095fab3c0bd8288de920db5e59
Darren Thompson <dthompson91@gmail.com>
2012-04-14 20:27:37 -0400

    Finish implementation of fortune, and finish implementing a dynamicly managed \!command list

--- src/Parse.py
+++ src/Parse.py
@@ -1,4 +1,5 @@
 import Filter
+from subprocess import check_output
 
 class Parse:
 	
@@ -6,27 +7,29 @@ class Parse:
 		self.bus = bus
 		self.chatNumber = chatNumber
 		self.commands = {'!test':"This is a test reply",
-				 '!colin':'brb in 10 min',
-				 '!nigger':"THAT'S RACIST!",
-				 '!petras':"Loves Pokemon more than his family",
-				 '!willy':"Is FABULOUS!",
-				 '!darren':"He is the all mighty creator!",
-				 '!dev':"I like trucks too",
-				 '!tase':"I'm afraid I can't do that Dave."}
+				 '!tase':"I'm afraid I can't do that.",
+				 '!hello':'Hello Dave',
+				 '!create':'',
+				 '!list':'',
+				 '!fortune':''}
 		
 	def Start(self, body):
-		if body[0].lower() == '!create' and not body[1].lower() == 'list':
+		if body[0].lower() == '!create':
 			string = ""
 			length = len(body) - 2
 			for i in range(length):
 				string = string + body[i+2] + " "
 			string.strip()
 			self.commands["!"+body[1].lower()] = string
+			self.Reply("Created !" + body[1])
 		elif body[0].lower() == "!list":
 			string = ""
 			for i in self.commands:
 				string = string + i + " "
 			self.Reply(string.lower())
+		elif body[0].lower() == "!fortune":
+			string = check_output('fortune')
+			self.Reply(string)
 		else:
 			for i in body:
 				if i.lower() in self.commands:
