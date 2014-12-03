9b70374a3f73be46fa96809a28231c1511b43791
Darren Thompson <dthompson91@gmail.com>
2014-12-03 16:50:44 -0500

    Removed str.lower() redundancies

--- src/Response.py
+++ src/Response.py
@@ -11,12 +11,12 @@ class ResponseGenerator:
 
     def generate_response(self, chat_name, body):
         # Tokenize body
-        body_tokens = body.split(" ")
+        body_tokens = body.lower().split(" ")
         # Important commands can only be run if line is started with the word
-        command = body_tokens[0].lower()
+        command = body_tokens[0]
 
         if command == '!create':
-            new_command = body_tokens[1].lower()
+            new_command = body_tokens[1]
             response_index = body.find(new_command) + len(new_command) + 1
             response = body[response_index:]
             self.commands.set(new_command, response)
@@ -28,7 +28,7 @@ class ResponseGenerator:
             for command_ in self.commands.list():
                 string += "!{0} ".format(command_)
 
-            self.reply(chat_name, string.lower())
+            self.reply(chat_name, string)
 
         elif command == "!delete":
             cleaned_command = body_tokens[1].lower()
@@ -47,19 +47,18 @@ class ResponseGenerator:
 
         # Not a system command, continue attempting to parse
         else:
-            for i in body_tokens:
-                word = i.lower()
-                if word == "!fortune":
+            for token in body_tokens:
+                if token == "!fortune":
                     # TODO
                     pass
-                elif word == "!excuse":
+                elif token == "!excuse":
                     self.reply(chat_name, self.excuses.get())
 
-                elif word == "!8ball":
+                elif token == "!8ball":
                     self.reply(chat_name, self.eightball.get())
 
-                elif word[0] == "!":
-                    self.reply(chat_name, self.commands.get(word[1:]))
+                elif token[0] == "!":
+                    self.reply(chat_name, self.commands.get(token[1:]))
 
     def reply(self, chat_name, message):
         string = "CHATMESSAGE " + chat_name + " " + message
